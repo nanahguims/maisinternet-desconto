3c26501079c8c3685a1b428f7cc82fcb33e71413
annythingelse <fernandaguimaraes0202@gmail.com>
2024-10-01 12:51:45 -0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,131 @@
-import Image from "next/image";
+'use client'
+import React, { useState } from 'react';
+import "./style.css";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [value, setValue] = useState(0);
+  const [selectedMonth, setSelectedMonth] = useState('');
+    const [plano, setPlano] = useState(0)
+    const [unit, setUnit] = useState('hours');
+    const [copySuccess, setCopySuccess] = useState('Copiar');
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+
+    const handleMonthChange = (e: any) => {
+      setSelectedMonth(e.target.value);
+    };
+
+    const handlePlanoChange = (e:any) => {
+        setPlano(e.target.value);
+    };
+
+    const handleValueChange = (e:any) => {
+        setValue(e.target.value);
+    };
+
+    const handleUnitChange = (e:any) => {
+        setUnit(e.target.value);
+    };
+
+
+    const getDaysInSelectedMonth = () => {
+      if (!selectedMonth) return 0;
+      
+      const [year, month] = selectedMonth.split('-').map(Number);
+      const daysInMonth = new Date(year, month, 0).getDate(); // Obtém o último dia do mês
+      return daysInMonth;
+    };
+
+
+    const calculate = () => {
+      if (!plano || !selectedMonth || value <= 0) {
+        return 0; 
+      }
+
+        const daysInCurrentMonth = getDaysInSelectedMonth(); 
+        const discountMouth = plano/daysInCurrentMonth
+  
+        if (unit === 'hours') {
+            const discountDay = discountMouth/24
+        
+            if(value <= 4) {
+                return discountDay*4
+            } else {
+                return discountDay*value;  
+            }
+        } else if (unit === 'days') {
+          return discountMouth * value;  
+        } 
+     
+    };
+
+      const formatToCurrency = (value:any) => {
+        return new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+          minimumFractionDigits: 3,
+        }).format(value);
+      };
+
+      const copyToClipBoard = async (copyMe: any) => {
+        try {
+          await navigator.clipboard.writeText(copyMe); 
+          setCopySuccess('Copied!');  
+
+        } catch (err) {
+          setCopySuccess('Failed to copy!');  
+        }
+
+        setTimeout(() => {
+          setCopySuccess('Copiar');  
+        }, 2000);
+      };
+    
+
+    return (
+        <div className='fundo'>
+          <div className='container'>
+            <div className='section'>
+              <div className='header'>
+                <img src="https://maisinternet.net.br/site/wp-content/uploads/2024/02/LOGO-NOVA-2-edited.png" alt="logo"/>
+                <h1>Calculadora de desconto</h1>
+              </div>
+              <div className='calculadora'>
+                <label> Valor do plano do cliente</label>
+                <input 
+                className='input-plano'
+                type="number"
+                value={plano}
+                onChange={handlePlanoChange}
+                />
+                <label> Mês que se trata</label>
+                <input type="month" onChange={handleMonthChange} />
+
+                <label>Quanto que o cliente ficou sem conexão </label>
+                <div className='container-input-desconto'>
+                    <input
+                    className='input-desconto'
+                    type="number"
+                    value={value}
+                    onChange={handleValueChange}
+                    />
+                    <select className='select-desconto' value={unit} onChange={handleUnitChange}>
+                      <option value="hours">Horas</option>
+                      <option value="days">Dias</option>
+                    </select>
+                </div>
+                  <label> Desconto</label>
+                  <div className='result'>
+                    <p>{formatToCurrency(calculate() || 0)}</p>
+                    <button 
+                      className='copy' 
+                      onClick={() => copyToClipBoard(formatToCurrency(calculate() || 0))}
+                    >
+                      {copySuccess}
+                    </button>
+                  </div>
+              </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+      </div>
+    );
 }
